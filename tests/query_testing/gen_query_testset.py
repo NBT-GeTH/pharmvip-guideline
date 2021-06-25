668420c58361ee4f01846fe32f77207f22ef981d
zenzero <zenzero.sohave@gmail.com>
2021-06-25 03:23:07 +0000

    init gen query class, test gen

--- tests/query_testing/gen_query_testset.py
+++ tests/query_testing/gen_query_testset.py
@@ -1,36 +1,60 @@
-#%%
 import random
-import glob
-from cyvcf2 import VCF
+from tests.matcher_testing.gen_matcher_testset import MatcherGenerator
 
+class QueryGenerator(MatcherGenerator):
 
-class QueryGenerator():
-    def  __init__(self) -> None:
-        pass
+    def  __init__(self,allele_definition_set=None, num_each_gene=2,missing_rate=0.25,false_rate_in_combine = 0.5):
+        super().__init__(allele_definition_set=allele_definition_set, num_each_gene=num_each_gene, missing_rate=missing_rate, false_rate_in_combine=false_rate_in_combine)
 
-# %%
-vcf_path = './data/NUDT15_dot.g.vcf.gz'
-vcf_test = './data/testest.vcf.gz'
-vcf = VCF(vcf_test)
-# 21167081
-#%%
-region = 'chr13:48037748-48037748'
-x = vcf(region)
+    # def  handle_gt_bases(self,gene, varian_pack, allele1, allele2, gt_phase):
+    #     if gt_phase :
+    #         phase = random.choice(['/','|']) if allele1 == allele2 else '|'
+    #         gene["gt_bases"] = f'{allele1}{phase}{allele2}'
+    #         gene["allele1"] = allele1
+    #         gene["allele2"] = allele2
 
-# %%
-header = {}
-header['fileformat'] = "##fileformat=VCFv4.2\n"
-header['source'] = '##source=SelectVariants\n'
-header['filter'] = '##FILTER=<ID=PASS,Description="All filters passed">\n'
-# header['info'] = '##INFO=<ID=PX,Number=.,Type=String,Description="PGX">\n'
-header['info'] = '##INFO=<ID=DP,Number=1,Type=Integer,Description="Approximate read depth; some reads may have been filtered">\n'
-header['format'] = '##FORMAT=<ID=GT,Number=1,Type=String,Description="Phased Genotype">\n'
-header['table_head'] = '#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tSAMPLE1\n'
-body = 'chr13	48037748	.	T	C	.	PASS	DP=21284;PX=;	GT:DP	./.:5\n'
+    def  gen_gene_variant(self, varian_pack, allele1, allele2, gt_phase):
+        haplotype = varian_pack[0]
+        var_inx = varian_pack[1]
+        dp_min = 0
+        dp_max = 1000
+        hgvs = haplotype["variants"][var_inx]["hgvs"]
+        hgvs_type = haplotype["variants"][var_inx]["hgvs_type"]
+        gene = {
+            "hgvs": hgvs,
+            "hgvs_type": hgvs_type,
+            "start": haplotype["variants"][var_inx]["start"],
+            "end": haplotype["variants"][var_inx]["end"],
+            "rsid": haplotype["variants"][var_inx]["rsid"],
+            "dp": int(random.uniform(dp_min,dp_max)),
+            "allele1_convert": allele1,
+            "allele2_convert": allele2,
+            "gt_phases": gt_phase
+        }
+        allele = allele1,allele2
+        phase = '/'
+        # phase = random.choice(['/','|']) if allele1 == allele2 else '|'
+        # gt_bases = f'{mini_a1}{phase}{mini_a2}'
+        # basic_base = ['A','T','C','G']
+        
+        if hgvs_type == 'INS' :
+            for i in (allele):
+                if i == 'del' : 
+            # mini_a1 = random.choice(basic_base)
+            # mini_a2 = random.choice(basic_base)
+            # gt_bases = f'{mini_a1}{allele1}{phase}{mini_a2}{allele2}'
+            
+        if gt_phase :
+            gene["gt_bases"] = 
+            gene["allele1"] = allele[1]
+            gene["allele2"] = allele[2]
+        
 
-# %%
-with open('./data/testest.vcf', "w") as f :
-    for key,val in header.items():
-        f.write(val)
-    f.write(body)
-# %%
+        return gene
+
+    def  sample_query_generator(self, gene_name, gene_phase, sample_id=0):
+        sample =  super().sample_query_generator(gene_name, gene_phase, sample_id=sample_id)
+        sample["chromosome"] = self.allele_definition_set[gene_name]["haplotypes"][0]["chromosome"]
+        del sample["haplotype_allele_name"]
+        return sample
+
