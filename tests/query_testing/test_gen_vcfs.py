602f09e63776b4bd0098f5a6112ce129ec251dec
zenzero <zenzero.sohave@gmail.com>
2021-06-29 06:14:36 +0000

    gen VCFs (INS )

--- tests/query_testing/test_gen_vcfs.py
+++ tests/query_testing/test_gen_vcfs.py
@@ -9,13 +9,13 @@ gener = VCFsFileGenerator(allele_definition_set)
 
 # %%
 gener.num_each_gene = 10
-gener.missing_rate = .2
-gener.massive_generation(gene_name='CACNA1S',gene_phase="True",id_prefix="TA")
+gener.missing_rate = .8
+gener.massive_generation(gene_name='CYP3A5',gene_phase="True",id_prefix="TA")
 # %%
 # gener.get_sample_collector()[0]
 
 #%%
-
+gener.from_query_set_to_VCFs(gener.get_sample_collector()[0])
 #%%
 #%%
 # f = open(dbpmcgenomics + "/allele_definitions_genome_at_POS.txt", "w")
@@ -35,3 +35,6 @@ x = vcf(region)
 # %%
 
 # %%
+
+# %%
+x
