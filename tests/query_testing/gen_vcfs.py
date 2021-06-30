45c193774daf65295f6b3a374de5337a2611097e
zenzero <zenzero.sohave@gmail.com>
2021-06-30 08:42:50 +0000

    done gen VCFs

--- tests/query_testing/gen_vcfs.py
+++ tests/query_testing/gen_vcfs.py
@@ -1,10 +1,12 @@
 # %%
+from ast import ExtSlice
 import random
 import glob
 import json
 import pickle
 import re 
 import os
+from typing import Type
 from pharmvip_guideline import *
 from tests.query_testing.gen_query_testset import QueryGenerator
 from pharmvip_guideline.utils.functional import import_allele_definition_set
@@ -31,7 +33,7 @@ class  VCFsFileGenerator(QueryGenerator):
         return data
 
     def  from_hgvs_to_possition(self,hgvs):
-        pos_finder = re.match((f'^g\.(\d+)(\_.*|\w.*)$'),hgvs)
+        pos_finder = re.match((r'^g\.(\d+)(\_.*|\w.*)$'),hgvs)
         return pos_finder.group(1)
 
     def  clear_body(self):
@@ -66,6 +68,7 @@ class  VCFsFileGenerator(QueryGenerator):
         return header
 
     def  find_genome_inx(self,genome_list,allele) :
+        allele = allele.replace('.','')
         try:
             genome_inx = genome_list.index(allele)
         except:
@@ -77,19 +80,21 @@ class  VCFsFileGenerator(QueryGenerator):
         return phase_pipe
 
     def  variant_to_VCF_body(self,variant,chrom):
-        pos = self.from_hgvs_to_possition(variant['hgvs'])
+        hgvs = variant['hgvs']
+        hgvs_start_pos = self.from_hgvs_to_possition(hgvs)
         dp = variant['dp']
         hgvs_type = variant['hgvs_type']
         ref = None
-        alt = self.possition_collector[pos]['alt']
+        alt = self.possition_collector[hgvs_start_pos]['alt']
+        phase = self.find_phase_pipe(variant['gt_bases'])
         if hgvs_type == "SNP" or hgvs_type == "CNV": #test by using CACNA1S
-            ref = self.possition_collector[pos]['ref']
+            ref = self.possition_collector[hgvs_start_pos]['ref']
             alt_fomat =  ','.join(alt)
             genome_list = [ref] + list(alt)
             genome1 = self.find_genome_inx(genome_list,variant['allele1'])
             genome2 = self.find_genome_inx(genome_list,variant['allele2'])
-            phase = self.find_phase_pipe(variant['gt_bases'])
-            body = f'{chrom}\t{pos}\t.\t{ref}\t{alt_fomat}\t.\tPASS\tPX=;\tGT:DP\t{genome1}{phase}{genome2}:{dp}\n'
+            # phase = self.find_phase_pipe(variant['gt_bases'])
+            body = f'{chrom}\t{hgvs_start_pos}\t.\t{ref}\t{alt_fomat}\t.\tPASS\tPX=;\tGT:DP\t{genome1}{phase}{genome2}:{dp}\n'
 
         if hgvs_type == 'INS': #test by using CYP3A5
             allele_list = []
@@ -102,21 +107,93 @@ class  VCFsFileGenerator(QueryGenerator):
                 ref = self.random_gt_base()
 
             allele_list = [ref if allele == 'del' else allele for allele in allele_list ]
-
             alt = [genome.replace('ins',ref) for genome in alt]
             alt_fomat =  ','.join(alt)
             genome_list = [ref] + list(alt)
             genome1 = self.find_genome_inx(genome_list,allele_list[0])
             genome2 = self.find_genome_inx(genome_list,allele_list[1])
-            phase = self.find_phase_pipe(variant['gt_bases'])
-            body = f'{chrom}\t{pos}\t.\t{ref}\t{alt_fomat}\t.\tPASS\tPX=;\tGT:DP\t{genome1}{phase}{genome2}:{dp}\n'
+            # phase = self.find_phase_pipe(variant['gt_bases'])
+            body = f'{chrom}\t{hgvs_start_pos}\t.\t{ref}\t{alt_fomat}\t.\tPASS\tPX=;\tGT:DP\t{genome1}{phase}{genome2}:{dp}\n'
+
+            
         
+        if hgvs_type == 'DEL':
+            ref = self.possition_collector[hgvs_start_pos]['ref']
+            genome_list = [ref] + list(alt)
+            genome1 = self.find_genome_inx(genome_list,variant['allele1_convert'])
+            genome2 = self.find_genome_inx(genome_list,variant['allele2_convert'])
+
+            find_which_del = re.match((r'^g\.(\d+)\_(\d+)del([A-Z]+)$'),hgvs)
+            if find_which_del == None :
+                find_which_del = re.match((r'^g\.(\d+)del([A-Z]+)$'),hgvs)
+                pos_list = int(find_which_del.group(1))
+                allele_list = find_which_del.group(2)
+                pos_befor = int(pos_list) - 1
+            else :
+                pos_list = [i for i in range(int(find_which_del.group(1)),int(find_which_del.group(2)) + 1)]
+                allele_list = find_which_del.group(3)
+                pos_befor = int(pos_list[0]) - 1
+
+            ref_base = None
+            for inx,allele in enumerate([variant['allele1'],variant['allele2']]):
+                if allele in self.basic_base:
+                    ref_base = allele
+            
+            if ref_base == None:
+                ref_base = self.random_gt_base()
+            
+            # for_extended_pos = genome_list[inx] 
+            for inx,allele in enumerate(genome_list):
+                if allele.__contains__('del'):
+                    genome_list[inx] = ref_base
+                else:
+                    genome_list[inx] = ref_base + genome_list[inx] 
+
+            ref_format = genome_list.pop(0)
+            alt_fomat =  ','.join(genome_list)
+            body = f'{chrom}\t{pos_befor}\t.\t{ref_format}\t{alt_fomat}\t.\tPASS\tPX=;\tGT:DP\t{genome1}{phase}{genome2}:{dp}\n'
+            alt = [ '*' * len(ref)  if allele.__contains__('del') else allele for allele in alt]
+            extend_alt =  ['*'] * len(ref)
+            extend_alt_inx1 = extend_alt_inx2 = [0] * len(ref)
+
+            for ainx,allele in enumerate(alt):
+                for binx,base in enumerate(allele):
+                    if (base not in ref[binx]) and (base not in extend_alt[binx]):
+                        extend_alt[binx] = extend_alt[binx] + base
+                        if genome1 - 1 == ainx : 
+                            extend_alt_inx1[binx] = len(extend_alt[binx]) - 1 
+                        if genome2 - 1 == ainx : 
+                            extend_alt_inx2[binx] = len(extend_alt[binx]) - 1 
+
+            if type(pos_list) == int :
+                inx = 0
+                body = body + f'{chrom}\t{pos_list}\t.\t{ref}\t{extend_alt[inx]}\t.\tPASS\tPX=;\tGT:DP\t{extend_alt_inx1[inx]}{phase}{extend_alt_inx1[inx]}:{dp}\n'
+            else:
+                for inx,poss in enumerate(pos_list):
+                    try:
+                        body = body + f'{chrom}\t{poss}\t.\t{ref[inx]}\t{extend_alt[inx]}\t.\tPASS\tPX=;\tGT:DP\t{extend_alt_inx1[inx]}{phase}{extend_alt_inx1[inx]}:{dp}\n'
+                    except:
+                        print(inx,ref,extend_alt,extend_alt_inx1,extend_alt_inx2)
+            # print(genome_list)
+            # print(ref)
+            # print(extend_alt)
+
+
+            # if find_which_del:
+            #     for inx,allele in enumerate([variant['allele1'],variant['allele2']]):
+            #         if not  allele.__contains__('.'):
+            #             raise print('lol:',hgvs, variant)
+            
+            # else:
+            #     find_which_del = re.match((r'^g\.(\d+)del([A-Z]+)$'),hgvs)
+            #     for inx,allele in enumerate([variant['allele1'],variant['allele2']]):
+            #         if not  allele.__contains__('.'):
+            #             raise print('lol:',hgvs, variant)
+                            
+                        # return 0
+            
         return body
 
-    # def  handle_body(self,chrom,pos,ref,alt,variant):
-    #     alt_fomat =  ','.join(alt)
-    #     genome_list = [ref] + list(alt)
-    #     genome1 = self.find_genome_inx(genome_list,variant['allele1'])
-    #     genome2 = self.find_genome_inx(genome_list,variant['allele2'])
-    #     phase = self.find_phase_pipe(variant['gt_bases'])
-    #     body = f'{chrom}\t{pos}\t.\t{ref}\t{alt_fomat}\t.\tPASS\tPX=;\tGT:DP\t{genome1}{phase}{genome2}:{dp}\n'
+
+#%%
+# %%
