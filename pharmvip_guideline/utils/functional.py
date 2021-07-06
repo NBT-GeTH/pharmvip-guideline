34b1317d5a9665fdc5b9d45de651a005e57aa2ba
zenzero <zenzero.sohave@gmail.com>
2021-07-06 04:05:36 +0000

    merg import allele def function

--- pharmvip_guideline/utils/functional.py
+++ pharmvip_guideline/utils/functional.py
@@ -2,9 +2,14 @@ import json
 import glob
 from pharmvip_guideline import *
 
-def  import_allele_definition_set():
+def  import_allele_definition_set(path:str=defaults_allele_definitions_transform,
+        exception:set = {'exemptions.json'} ):
     allele_definition_set = {}
-    for allele_definition_json in glob.glob(defaults_allele_definitions_transform + "/*.json"):
+    for allele_definition_json in glob.glob(path + "/*.json"):
+        file_name = os.path.basename(allele_definition_json)
+        if file_name in exception :
+            continue
+
         allele_definition = json.load(open(allele_definition_json))
         allele_definition_set[allele_definition["gene"]] = allele_definition
     return allele_definition_set
