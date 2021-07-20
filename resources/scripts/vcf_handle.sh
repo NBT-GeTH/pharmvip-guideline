8c7ec2b32db558d7dfcf9ecf24babceb925f112d
zenzero <zenzero.sohave@gmail.com>
2021-07-20 07:44:09 +0000

    add some command

--- resources/scripts/vcf_handle.sh
+++ resources/scripts/vcf_handle.sh
@@ -1,10 +1,34 @@
+#!/bin/bash
+# while getopts u:a:f: flag
+# do
+#     case "${flag}" in
+#         u) username=${OPTARG};;
+#         a) age=${OPTARG};;
+#         f) fullname=${OPTARG};;
+#     esac
+# done
+
+# FILES="./data/*.vcf"
+# # FILES2="./data/tester.vcf"
+# for f in $FILES
+# do
+#     echo "Processing ${f} file..."
+#     bgzip < ${f} > "${f}.gz"
+#     tabix -p vcf "${f}.gz" 
+# done
+
+# #!/bin/bash
+# FILES="./bigchunk/*.vcf.gz"
+# for f in $FILES
+# do
+#     echo "Processing ${f} file..."
+#     gunzip -k -f ${f}
+# done
 
 #!/bin/bash
-FILES="./data/*.vcf"
-# FILES2="./data/tester.vcf"
+FILES="./bigchunk/*.vcf"
 for f in $FILES
 do
     echo "Processing ${f} file..."
-    bgzip < ${f} > "${f}.gz"
-    tabix -p vcf "${f}.gz" 
+    gzip -k  ${f}
 done
