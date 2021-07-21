2be4ce2e8c6506009f3c64bd0cc00cb3497e4b80
zenzero <zenzero.sohave@gmail.com>
2021-07-21 07:37:48 +0000

    update vcf script

--- resources/scripts/vcf_handle.sh
+++ resources/scripts/vcf_handle.sh
@@ -8,27 +8,52 @@
 #     esac
 # done
 
-# FILES="./data/*.vcf"
+# FILES="../samples/1000t/*.vcf"
 # # FILES2="./data/tester.vcf"
 # for f in $FILES
 # do
 #     echo "Processing ${f} file..."
-#     bgzip < ${f} > "${f}.gz"
-#     tabix -p vcf "${f}.gz" 
+#     # bgzip < ${f} > "${f}.gz"
+#     # gunzip -k -f ${f}
+#     # tabix -p vcf "${f}.gz" 
 # done
 
-# #!/bin/bash
-# FILES="./bigchunk/*.vcf.gz"
+# FILES="../samples/1000t/*.vcf.gz"
+# for f in $FILES
+# do
+#     SUBSTRING=$(echo  ${f} | rev | cut -c4- | rev)
+#     echo $SUBSTRING
+#     if test -f $SUBSTRING; then
+#         echo "${SUBSTRING} exists."
+#     else 
+#         gunzip -k -f $SUBSTRING
+#         echo "${SUBSTRING} does not exist."
+#     fi
+# done
+
+
+FILES="../samples/1000t/*.vcf"
+for f in $FILES
+do
+    SUBSTRING=$(echo  "${f}.gz.tbi")
+    echo $SUBSTRING
+    if test -f $SUBSTRING; then
+        echo "${SUBSTRING} exists."
+    else 
+        echo "${SUBSTRING} does not exist."
+        bgzip < ${f} > "${f}.gz"
+        tabix -p vcf "${f}.gz" 
+
+    fi
+    # bgzip < ${f} > "${f}.gz"
+    # gunzip -k -f ${f}
+    # tabix -p vcf "${f}.gz" 
+done
+
+#!/bin/bash
+# FILES="./bigchunk/*.vcf"
 # for f in $FILES
 # do
 #     echo "Processing ${f} file..."
-#     gunzip -k -f ${f}
+#     gzip -k  ${f}
 # done
-
-#!/bin/bash
-FILES="./bigchunk/*.vcf"
-for f in $FILES
-do
-    echo "Processing ${f} file..."
-    gzip -k  ${f}
-done
