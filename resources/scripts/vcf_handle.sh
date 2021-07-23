9a42d2f58379ba068a97bd173b8295c03d845f96
zenzero <zenzero.sohave@gmail.com>
2021-07-23 05:09:50 +0000

    update vcf hadle bashscript

--- resources/scripts/vcf_handle.sh
+++ resources/scripts/vcf_handle.sh
@@ -1,59 +1,70 @@
 #!/bin/bash
-# while getopts u:a:f: flag
-# do
-#     case "${flag}" in
-#         u) username=${OPTARG};;
-#         a) age=${OPTARG};;
-#         f) fullname=${OPTARG};;
-#     esac
-# done
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo "Add description of the script functions here."
+   echo
+   echo "Syntax: scriptTemplate [-g|d] [PATH]"
+   echo "options:"
+   echo "-h,--help      Print Help."
+   echo "-d,--decompose decompose .vcf.gz."
+   echo "-i|--indexe    from vcf create vcf.gz and vcf.gz.tbi"
+   echo
+}
 
-# FILES="../samples/1000t/*.vcf"
-# # FILES2="./data/tester.vcf"
-# for f in $FILES
-# do
-#     echo "Processing ${f} file..."
-#     # bgzip < ${f} > "${f}.gz"
-#     # gunzip -k -f ${f}
-#     # tabix -p vcf "${f}.gz" 
-# done
+Ungz() {
+    FILES="${INPP}/*.vcf.gz"
+    for f in $FILES
+    do
+        echo "Processing ${f} file..."
+        SUBSTRING=$(echo  ${f} | rev | cut -c4- | rev)
+        gzip  -d <  ${f} > ${SUBSTRING}
+    done
+}
 
-# FILES="../samples/1000t/*.vcf.gz"
-# for f in $FILES
-# do
-#     SUBSTRING=$(echo  ${f} | rev | cut -c4- | rev)
-#     echo $SUBSTRING
-#     if test -f $SUBSTRING; then
-#         echo "${SUBSTRING} exists."
-#     else 
-#         gunzip -k -f $SUBSTRING
-#         echo "${SUBSTRING} does not exist."
-#     fi
-# done
-
-
-FILES="../samples/1000t/*.vcf"
-for f in $FILES
-do
-    SUBSTRING=$(echo  "${f}.gz.tbi")
-    echo $SUBSTRING
-    if test -f $SUBSTRING; then
-        echo "${SUBSTRING} exists."
-    else 
-        echo "${SUBSTRING} does not exist."
-        bgzip < ${f} > "${f}.gz"
+IndexManipulate() {
+    FILES="${INPP}/*.vcf"
+    for f in $FILES
+    do
+        echo "Processing ${f} file..."
+        gzip -f < ${f} > "${f}.gz"
         tabix -p vcf "${f}.gz" 
+    done
+}
 
-    fi
-    # bgzip < ${f} > "${f}.gz"
-    # gunzip -k -f ${f}
-    # tabix -p vcf "${f}.gz" 
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -d|--decompose)
+      INPP="$2"
+      shift # past argument
+      shift # past value
+      Ungz
+      ;;
+    -h|--help)
+      SEARCHPATH="$2"
+      shift # past argument
+      shift # past value
+      Help
+      ;;
+    -i|--indexe)
+      INPP="$2"
+      shift # past argument
+      shift # past value
+      IndexManipulate
+      ;;
+    --default)
+      DEFAULT=YES
+      shift # past argument
+      ;;
+    *)    # unknown option
+      POSITIONAL+=("$1") # save it in an array for later
+      shift # past argument
+      Help
+      ;;
+  esac
 done
-
-#!/bin/bash
-# FILES="./bigchunk/*.vcf"
-# for f in $FILES
-# do
-#     echo "Processing ${f} file..."
-#     gzip -k  ${f}
-# done
