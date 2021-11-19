2ee7304a3d9168a8f14fafe5dd09f3b94a2a6f31
Kwankom Traiparn <ktraipar@tara-frontend-1.tara.nstda.or.th>
2021-11-19 12:15:56 +0700

    update allele def and change version

--- pharmvip_guideline/__init__.py
+++ pharmvip_guideline/__init__.py
@@ -1,7 +1,7 @@
 import os
 current_version = "2020_12_08_v0_6_0_cftr_dpyd_edited"
-trial_version = "2021_7_8"
-allele_definitions_table_version = current_version
+trial_version = "2021_11_19"
+allele_definitions_table_version = trial_version
 defaults_allele_definitions_table = os.path.join(os.path.join(os.path.dirname(__file__), ".."), "resources", "allele_definitions", allele_definitions_table_version, "table")
 defaults_allele_definitions_transform = os.path.join(os.path.join(os.path.dirname(__file__), ".."), "resources", "allele_definitions", allele_definitions_table_version, "transform")
 defaults_allele_definitions_dbpmcgenomics = os.path.join(os.path.join(os.path.dirname(__file__), ".."), "resources", "allele_definitions", allele_definitions_table_version, "dbpmcgenomics")
