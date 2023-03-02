4e3c2a558dae7d51fb2f1fdc54bb911ac1d23001
Zentriole <zenzero.sohave@gmail.com>
2023-03-02 15:28:29 +0700

    edit debugger

--- tests/unitest/overall_debugger.py
+++ tests/unitest/overall_debugger.py
@@ -2,10 +2,10 @@ import sys
 from pharmvip_guideline import main
 from pharmvip_guideline import pack_path
 import sys
-sample_path = pack_path + '/../resources/samples/HS10227/'
-vcf_path = sample_path + 'HS10227_final.vcf.gz'
-diplotype_cyp2d6_path = sample_path + 'HS10227_diplotype_CYP2D6.tsv'
-diplotype_hla_path = sample_path + 'HS10227_diplotype_hla.tsv'
+sample_path = pack_path + '/../resources/samples/NA12878_30x/'
+vcf_path = sample_path + 'NA12878_30x.vcf.gz'
+diplotype_cyp2d6_path = sample_path + 'NA12878_30x_diplotype_CYP2D6.tsv'
+diplotype_hla_path = sample_path + 'NA12878_30x_diplotype_hla.tsv'
 outpath = sample_path + '/../../../.out'
 para_paser = ['allele_matcher',"--ana_user_id",'1','--ana_id',
     '1','--ana_options_cpic','true','--ana_best_candidate','false',
